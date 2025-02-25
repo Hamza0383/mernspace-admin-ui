c8a0e22bb54a1444262daa82a48d383298295b76
Hamza khan <m.hamzakhaan@gmail.com>
2025-02-25 20:57:20 +0500

    first component test

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-import Categories from "./pages/Categories";
+import LoginPage from "./pages/login/LoginPage";
 
 export const router = createBrowserRouter([
   { path: "/", element: <Homepage /> },
-  { path: "/categories", element: <Categories /> },
+  { path: "/auth/login", element: <LoginPage /> },
 ]);
